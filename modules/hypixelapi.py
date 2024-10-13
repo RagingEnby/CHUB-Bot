e4dd5c3b03d2d0faf8416b6ad0be65c2ff132b08
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-13 22:48:58 +0000

    improved hypixelapi.ensure_data()'s structure to make it more readible

--- modules/hypixelapi.py
+++ modules/hypixelapi.py
@@ -8,7 +8,10 @@ from modules import asyncreqs
 import config
 import ws
 
+PLAYER_RATE_LIMIT_MSG = "You have already looked up this player too recently, please try again shortly"
 API_URL = "https://api.hypixel.net/v2"
+
+LAST_RESPONSE: dict[str, dict] = {}
 
 
 async def get_data(
@@ -20,9 +23,10 @@ async def get_data(
         params['key'] = config.HYPIXEL_API_KEY
     url = f"{API_URL}{endpoint}"
     response = await asyncreqs.get(url, params=params, session=session)
+    data = await response.json()
     if response.status == 200:
         ws.queue.append({
-            "data": await response.json(),
+            "data": data,
             "params": params or {},
             "url": url
         })
@@ -32,12 +36,29 @@ async def get_data(
 async def ensure_data(endpoint: str,
                       params: Optional[dict] = None,
                       session: Optional[aiohttp.ClientSession] = None) -> dict:
+    global LAST_RESPONSE
+    id_ = params.get('uuid', params.get('player', params.get('id'))) if params else None
     response = await get_data(endpoint, params, session=session)
-    if response.status not in [200, 204]:
-        if response.status != 429:
-            print(
-                f'received response {response.status} from {endpoint} (params: {json.dumps(params)})'
-            )
-        await asyncio.sleep(5)
-        return await ensure_data(endpoint, params, session=session)
-    return await response.json()
+    if response.status == 200 or response.status == 204:
+        data = await response.json()
+        if id_:
+            LAST_RESPONSE[id_] = data
+        return data
+
+    if response.status == 429:
+        data = await response.json()
+        if data.get('cause') == PLAYER_RATE_LIMIT_MSG:
+            if id_ in LAST_RESPONSE:
+                return LAST_RESPONSE[id_]
+            await asyncio.sleep(35)
+            return await ensure_data(endpoint, params, session=session)
+        important_headers = {k: v for k, v in dict(response.headers).items() if k.startswith('ratelimit-')}
+        print('RATE LIMITED!!!')
+        print('important_headers', json.dumps(important_headers, indent=2))
+        print('data', json.dumps(data, indent=2))   
+    else:
+        print(f'received response {response.status} from {endpoint} (params: {json.dumps(params)})')
+        
+    await asyncio.sleep(5)
+    return await ensure_data(endpoint, params, session=session)
+    
