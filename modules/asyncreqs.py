3d9271c846c488ee3bea0dd58e26929cf5794487
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-13 22:14:44 +0000

    added a rate limit warning to console

--- modules/asyncreqs.py
+++ modules/asyncreqs.py
@@ -8,5 +8,7 @@ async def get(*args, **kwargs) -> aiohttp.ClientResponse:
             return await get(*args, session=session, **kwargs)
     async with session.get(*args, **kwargs) as response:
         await response.text()  # wait for response to go through (async can be buggy so this is somewhat needed)
+        if response.status == 429 and 'api.hypixel.net' in args[0]:
+            print('RATE LIMITED!!!')
         return response
         
