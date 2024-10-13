11a2546dd77d1e9d5825e53bc3e583f0736f709c
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-13 22:25:15 +0000

    now prints less headers than before when rate limited

--- modules/asyncreqs.py
+++ modules/asyncreqs.py
@@ -10,7 +10,8 @@ async def get(*args, **kwargs) -> aiohttp.ClientResponse:
     async with session.get(*args, **kwargs) as response:
         await response.text()  # wait for response to go through (async can be buggy so this is somewhat needed)
         if response.status == 429 and 'api.hypixel.net' in args[0]:
+            important_headers = {k: v for k, v in dict(response.headers).items() if k.startswith('ratelimit-')}
             print('RATE LIMITED!!!')
-            print(json.dumps(dict(response.headers), indent=2))
+            print(json.dumps(important_headers, indent=2))
         return response
         
