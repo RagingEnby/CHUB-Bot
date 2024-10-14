c23d3e3d47134ba478ca97b1036d1f747d124282
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-14 02:31:39 +0000

    fixed the STUPID FUCKING BUG IM SO DUMB HOLY SHIT

--- modules/verifier.py
+++ modules/verifier.py
@@ -71,13 +71,7 @@ async def get_item_roles(player: datatypes.MinecraftPlayer, session: Optional[ai
     item_roles = []
     items = await misc.get_player_items(player.uuid, session=session)
     item_ids = [item['ExtraAttributes']['id'] for item in items.values()]
-    # really shittily made debug statement, ignore it:
-    for item in items.values():
-        if isinstance(item, str):
-            print("item, str", json.dumps(item, indent=2))
-        elif isinstance(item.get('ExtraAttributes'), str):
-            print("ExtraAttributes, str", json.dumps(item, indent=2))
-    pets = [json.loads(item['ExtraAttributes']['petInfo']) for item in items if item.get('ExtraAttributes', {}).get('id') == 'PET']
+    pets = [json.loads(item['ExtraAttributes']['petInfo']) for item in items.values() if item.get('ExtraAttributes', {}).get('id') == 'PET']
     pet_skins = list(set(['PET_SKIN_' + pet['skin'] for pet in pets if pet['skin']]))
     item_ids.extend(pet_skins)
     item_ids = list(set(item_ids)) # remove duplicates
