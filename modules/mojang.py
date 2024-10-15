14fea399a565cc950895a3f80147b0079e25a65c
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-15 02:43:25 +0000

    added the ability to using api.ragingenby.dev if rate limited by mojang

--- modules/mojang.py
+++ modules/mojang.py
@@ -1,18 +1,28 @@
-from typing import Optional
+from typing import Optional, Literal
 import aiohttp
 import datatypes
 import aiohttp.client_exceptions
 
 from modules import asyncreqs
 
+Api = Literal['mojang', 'ragingenby']
 
-async def get_uuid(name: str, session: Optional[aiohttp.ClientSession] = None) -> Optional[datatypes.MinecraftPlayer]:
-    url = 'https://api.mojang.com/users/profiles/minecraft/' + name
+
+async def get_uuid(name: str, session: Optional[aiohttp.ClientSession] = None,
+                   api: Api = 'mojang') -> Optional[datatypes.MinecraftPlayer]:
+    url = ('https://api.mojang.com/users/profiles/minecraft/'
+           if api == 'mojang' else 'https://api.ragingenby.dev/player/') + name
     response = await asyncreqs.get(url, session=session)
+    if response.status == 429 and api == 'mojang':
+        return await get_uuid(name, session=session, api='ragingenby')
     return datatypes.MinecraftPlayer.from_dict(await response.json())
 
 
-async def get_name(uuid: str, session: Optional[aiohttp.ClientSession] = None) -> Optional[datatypes.MinecraftPlayer]:
-    url = 'https://sessionserver.mojang.com/session/minecraft/profile/' + uuid
+async def get_name(uuid: str, session: Optional[aiohttp.ClientSession] = None,
+                   api: Api = 'mojang') -> Optional[datatypes.MinecraftPlayer]:
+    url = ('https://sessionserver.mojang.com/session/minecraft/profile/'
+           if api == 'mojang' else 'https://api.ragingenby.dev/player/') + uuid
     response = await asyncreqs.get(url, session=session)
+    if response.status == 429 and api == 'mojang':
+        return await get_uuid(uuid, session=session, api='ragingenby')
     return datatypes.MinecraftPlayer.from_dict(await response.json())
