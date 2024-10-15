8d46b6dbd129c008504c3c6bf43fe9834ce4c1a4
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-15 06:48:46 +0000

    fixed asyncreqs.get() crashing with no session and added a warning msg

--- modules/asyncreqs.py
+++ modules/asyncreqs.py
@@ -2,8 +2,9 @@ import aiohttp
 
 
 async def get(*args, **kwargs) -> aiohttp.ClientResponse:
-    session = kwargs.pop('session')
+    session = kwargs.pop('session') if 'session' in kwargs else None
     if session is None:
+        print('WARNING: Request to', args[0], 'is being made with no session, this will slow the bot down. Ensure this is intentional.')
         async with aiohttp.ClientSession() as session:
             return await get(*args, session=session, **kwargs)
     async with session.get(*args, **kwargs) as response:
