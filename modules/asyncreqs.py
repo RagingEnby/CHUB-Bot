f2c4448a4524813de8dc69d4e4a8e05d73f02e43
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-15 07:17:44 +0000

    updated asyncreqs.get() warning to work with new api version of api.ragingenby.dev

--- modules/asyncreqs.py
+++ modules/asyncreqs.py
@@ -6,7 +6,7 @@ async def get(*args, **kwargs) -> aiohttp.ClientResponse:
     if session is None:
         # this api request has to be done with no session because of how disnake
         # autocomplete works
-        if args[0] != 'https://api.ragingenby.dev/stem':
+        if not args[0].startswith('https://api.ragingenby.dev/stem'):
             print('WARNING: Request to', args[0], 'is being made with no session,'
                   'this will slow the bot down. Ensure this is intentional.')
         async with aiohttp.ClientSession() as session:
