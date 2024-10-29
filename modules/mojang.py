32bb46c5c7ab50dffcdee354971f2ca9d615b503
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2024-10-29 18:12:15 +0000

    switched modules/mojang.py to be better (api.ragingenby.dev no longer on VPS)

--- modules/mojang.py
+++ modules/mojang.py
@@ -1,49 +1,18 @@
-from typing import Optional, Literal
-from enum import Enum
+from typing import Optional
 import aiohttp
 import datatypes
-import aiohttp.client_exceptions
 
 from modules import asyncreqs
 
-class Api(Enum):
-    RAGING = 1
-    MOJANG = 2
 
-
-def make_url(identifier: str, api: Api) -> str:
-    if api == Api.MOJANG and len(identifier) >= 32:
-        # identifier is a UUID, use mojang's UUID -> Name API
-        url = 'https://sessionserver.mojang.com/session/minecraft/profile/'
-    elif api == Api.MOJANG and len(identifier) < 32:
-        # identifier is a Name, use mojang's Name -> UUID API
-        url = 'https://api.mojang.com/users/profiles/minecraft/'
-    else:
-        # identifier is a Name or UUID, use ragingenby's Name <-> UUID API
-        url = 'https://api.ragingenby.dev/player/'
-    return url + identifier
-
-
-async def get(identifier: str, session: Optional[aiohttp.ClientSession] = None,
-                   api: Api = Api.MOJANG) -> Optional[datatypes.MinecraftPlayer]:
-    url = make_url(identifier, api)
-    response = await asyncreqs.get(url, session=session)
-    try:
-        if response.status == 429:
-            raise Exception('Rate limited')
-        return datatypes.MinecraftPlayer.from_dict(await response.json())
-    except Exception as e:
-        if api == Api.MOJANG:
-            print(f'mojang.get({identifier}, api=Api.MOJANG) raised:', e)
-            return await get(identifier, session=session, api=Api.RAGING)
-        raise e # raise the error if this is from api.ragingenby.dev
-
-
-async def bulk_uuid_to_player(uuids: list[str], session: Optional[aiohttp.ClientSession] = None) -> list[datatypes.MinecraftPlayer]:
-    body = {
-        "uuids": uuids
-    }
-    response = await asyncreqs.post("https://api.ragingenby.dev/names", body=body, session=session)
+async def get(
+    identifier: str,
+    session: Optional[aiohttp.ClientSession] = None,
+) -> Optional[datatypes.MinecraftPlayer]:
+    response = await asyncreqs.get('https://api.ragingenby.dev/player/' +
+                                   identifier,
+                                   session=session)
     data = await response.json()
-    return [datatypes.MinecraftPlayer.from_dict(player) for player in data['players']]
-    
+    if 'id' in data and 'name' in data:
+        return datatypes.MinecraftPlayer.from_dict(data)
+    return None
