b76fb42162bebdfb331cfbc3a4e9b4640460022f
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2025-02-03 05:21:36 +0000

    requests with closed sessions will function properly now

--- modules/asyncreqs.py
+++ modules/asyncreqs.py
@@ -3,7 +3,7 @@ import aiohttp
 
 async def get(*args, **kwargs) -> aiohttp.ClientResponse:
     session = kwargs.pop('session', None)
-    if session is None:
+    if session is None or session.closed:
         # this api request has to be done with no session because of how disnake
         # autocomplete works
         #if not args[0].startswith('https://api.ragingenby.dev/stem'):
@@ -11,22 +11,15 @@ async def get(*args, **kwargs) -> aiohttp.ClientResponse:
         #          'this will slow the bot down. Ensure this is intentional.')
         async with aiohttp.ClientSession() as session:
             return await get(*args, session=session, **kwargs)
-
-    async with session.get(*args, **kwargs) as response:
-        await response.read() # wait for response (async is buggy)
-
-        # this is semi temp. its made to monitor that excessive requests arent being sent
-        """if 'api.hypixel.net' in args[0]:
-            params = kwargs.get('params', {})
-            # wow holy SHIT this print statement is ugly but i cba to make it better
-            # also this makes it comply with pRoPEr PrACtiCeS
-            print(response.status, args[0].split('.net')[-1] + ' - ' +
-                params.get('uuid',
-                    params.get('player',
-                        params.get('profile', 
-                            params.get('id', json.dumps(params))))))"""
-        return response
-
+    try:
+        async with session.get(*args, **kwargs) as response:
+            await response.read() # wait for response (async is buggy)
+            return response
+    except RuntimeError as e:
+        if str(e) == "Session is closed":
+            async with aiohttp.ClientSession() as session:
+                return await get(*args, session=session, **kwargs)
+        raise e
 
 async def post(*args, **kwargs) -> aiohttp.ClientResponse:
     session = kwargs.pop('session', None)
