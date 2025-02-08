79cfe75e5ad666116c5a7c15ae10a4a2424ffdb0
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2025-02-08 20:20:08 +0000

    added ChubBot user agent for requests (should've done this a LONG time ago oops)

--- modules/asyncreqs.py
+++ modules/asyncreqs.py
@@ -1,24 +1,22 @@
 import aiohttp
 
+import config
+
 
 async def get(*args, **kwargs) -> aiohttp.ClientResponse:
     session = kwargs.pop('session', None)
+    headers = kwargs.pop('headers', {})
+    headers['User-Agent'] = config.USER_AGENT
     if session is None or session.closed:
-        # this api request has to be done with no session because of how disnake
-        # autocomplete works
-        #if not args[0].startswith('https://api.ragingenby.dev/stem'):
-        #    print('WARNING: Request to', args[0], 'is being made with no session,'
-        #          'this will slow the bot down. Ensure this is intentional.')
         async with aiohttp.ClientSession() as session:
             return await get(*args, session=session, **kwargs)
     try:
-        async with session.get(*args, **kwargs) as response:
-            await response.read() # wait for response (async is buggy)
+        async with session.get(*args, headers=headers, **kwargs) as response:
+            await response.read() # read the response before we __aexit__ so itll store
             return response
     except RuntimeError as e:
         if str(e) == "Session is closed":
-            async with aiohttp.ClientSession() as session:
-                return await get(*args, session=session, **kwargs)
+            return await get(*args, **kwargs)
         raise e
 
 
--- modules/mojang.py
+++ modules/mojang.py
@@ -1,19 +1,26 @@
 from typing import Optional
+import asyncio
 
 import aiohttp
 
 import datatypes
 from modules import asyncreqs
+import config
 
 
 async def get(
     identifier: str,
     session: Optional[aiohttp.ClientSession] = None,
 ) -> Optional[datatypes.MinecraftPlayer]:
+    url = 'https://api.ragingenby.dev/player/' + identifier\
+            if 'https://' not in identifier\
+            else identifier
+    
     response = await asyncreqs.get(
-        'https://api.ragingenby.dev/player/' + identifier,
+        url,
         session=session
     )
+        
     data = await response.json()
     if 'id' in data and 'name' in data:
         return datatypes.MinecraftPlayer.from_dict(data)
