7f6a9d111827ae7e22a14d3f8bd340dbaf21aac8
RagingEnby <71851438+RagingEnby@users.noreply.github.com>
2025-02-26 17:26:05 +0000

    way decreased scan rate

--- main.py
+++ main.py
@@ -693,7 +693,7 @@ async def update_linked_players_task():
             elif discord_id == bot.user.id:
                 continue
             await update_players_task_single(uuid, member_dict[discord_id], session=session)
-            await asyncio.sleep(2.75)  # rate limit sucks ass
+            await asyncio.sleep(10)  # rate limit sucks ass
 
 
 TSKS.append(update_linked_players_task)
