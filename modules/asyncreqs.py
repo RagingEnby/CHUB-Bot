caff393cd5008319916ad66e1644229b50cdea4b
RagingEnby <github@ragingenby.dev>
2025-12-08 20:56:23 -0500

    request debugging

--- modules/asyncreqs.py
+++ modules/asyncreqs.py
@@ -4,17 +4,23 @@ import config
 
 
 async def get(*args, **kwargs) -> aiohttp.ClientResponse:
-    session = kwargs.pop('session', None)
-    headers = kwargs.pop('headers', {})
+    session = kwargs.pop("session", None)
+    headers = kwargs.pop("headers", {})
     if session is None or session.closed:
         async with aiohttp.ClientSession() as session:
             return await get(*args, session=session, **kwargs)
-    if 'api.hypixel.net' in args[0]:
-        kwargs['proxy'] = config.PROXY
-        kwargs['proxy_auth'] = config.PROXY_AUTH
+    if "api.hypixel.net" in args[0]:
+        kwargs["proxy"] = config.PROXY
+        kwargs["proxy_auth"] = config.PROXY_AUTH
     try:
         async with session.get(*args, headers=headers, **kwargs) as response:
-            await response.read() # read the response before we __aexit__ so itll store
+            await response.read()  # read the response before we __aexit__ so itll store
+            if "api.hypixel.net" in response.url.host:
+                print(
+                    response.status,
+                    response.method,
+                    str(response.url).split(".net/")[-1],
+                )
             return response
     except RuntimeError as e:
         if str(e) == "Session is closed":
@@ -23,11 +29,10 @@ async def get(*args, **kwargs) -> aiohttp.ClientResponse:
 
 
 async def post(*args, **kwargs) -> aiohttp.ClientResponse:
-    session = kwargs.pop('session', None)
+    session = kwargs.pop("session", None)
     if session is None:
         async with aiohttp.ClientSession() as session:
             return await post(*args, session=session, **kwargs)
     async with session.get(*args, **kwargs) as response:
         await response.read()
         return response
-        
